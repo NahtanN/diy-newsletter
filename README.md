dc6a226bcf463dff1b0d43b9daf9644c2edb645b
Nathan Gomes <nathan.neri@al.senai.br>
2024-06-27 20:49:15 -0300

    fix: chatgpt prompt

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ### TODO:
 
-- [ ] fix scraped_articles and crawled_urls relation
+- [x] fix scraped_articles and crawled_urls relation
   - should go from ManytoOne/OneToMany to ManyToMany
-- [ ] fix create article with chatGPT
+- [x] fix create article with chatGPT
 - [ ] add list newsletters
   - [ ] all
   - [ ] by preference
+  - [ ] find one
 - [ ] create docs
   - [ ] swagger
   - [ ] docker/docker compose
--- src/modules/ai/ai.service.ts
+++ src/modules/ai/ai.service.ts
@@ -3,50 +3,48 @@ import OpenAI from 'openai';
 
 @Injectable()
 export class AiService {
-  private readonly logger = new Logger(AiService.name);
-  private openai: OpenAI;
+ private readonly logger = new Logger(AiService.name);
+ private openai: OpenAI;
 
-  constructor() {
-    this.openai = new OpenAI({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
-  }
+ constructor() {
+  this.openai = new OpenAI({
+   apiKey: process.env.OPENAI_API_KEY,
+  });
+ }
 
-  async createAIContent(data: { title: string; articles: string[] }) {
-    return { newsletter: 'placeholder' };
-    try {
-      const completion = await this.openai.chat.completions.create({
-        messages: [
+ async createAIContent(data: { title: string; articles: string[] }) {
+  try {
+   const completion = await this.openai.chat.completions.create({
+    messages: [
+     {
+      role: 'system',
+      content: `Você é um assitente que ajuda a criar resumos de artigos fornecidos a você no formato markdown.
+          O usuário fornecerá os dados em um objeto JSON no seguinte formato.
           {
-            role: 'system',
-            content: `Você é um assitente que ajuda a criar newsletters personalizadas de acordo com o conteúdo de artigos
-                    fornecidos a você, no formato markdown. O usuário fornecerá os dados no seguinte formato.
-                        {
-                        "title": "título da newsletter",
-                        "data": [
-                            "conteúdo do artigo",
-                            "conteúdo do artigo"
-                        ]
-                    }
+            "title": "título da newsletter",
+            "data": [
+                "conteúdo do artigo",
+                "conteúdo do artigo"
+            ]
+          }
 
-                    O seu papel será criar uma newsletter personalizada, de acordo com os artigos. Tente agrupar conteúdos
-                    semelhantes, resumir informação e criar insights no formato bullet point para alguns conteúdos.
+    O seu papel será criar resumos, de acordo com o conteúdo principal de cada artigo. Extraia o 
+    conteúdo principal, resuma as informação do conteúdo principal e criar insights no formato bullet 
+    point para cada artigo. Faça isso para cada artigo e agrupe os resultados em um texto markdown.
 
-                        O artigo será retornado no formato markdown, dentro do objeto json, como no exemplo:
-                    {
-                        "newsletter": "conteúdo da newsletter no formato markdown"
-                    }`,
-          },
-          { role: 'user', content: JSON.stringify(data) },
-        ],
-        model: 'gpt-4o',
-        response_format: { type: 'json_object' },
-      });
+    O resumo de todos os artigos será retornado no formato markdown, dentro de um objeto JSON, como no exemplo:
+        { "newsletter": "conteúdo do resumo dos artigos no formato markdown" }
+    `,
+     },
+     { role: 'user', content: JSON.stringify(data) },
+    ],
+    model: 'gpt-4o',
+    response_format: { type: 'json_object' },
+   });
 
-      console.log(completion.choices[0].message.content);
-      return JSON.parse(completion.choices[0].message.content) as { newsletter: string };
-    } catch (error) {
-      this.logger.error(`Não foi possível criar o conteúdo da newsletter: ${error}`);
-    }
+   return JSON.parse(completion.choices[0].message.content) as { newsletter: string };
+  } catch (error) {
+   this.logger.error(`Não foi possível criar o conteúdo da newsletter: ${error}`);
   }
+ }
 }
